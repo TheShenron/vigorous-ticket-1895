ed0b073465208611538c28d43b21d0a8e60b04aa
theshenron <Gourav143faz@gmail.com>
2022-09-30 15:35:24 +0530

    working on Login section

---
+++ src/Components/day_4/LoginComp.jsx
@@ -0,0 +1,12 @@
+import { Container, Icon } from '@chakra-ui/react';
+import React from 'react';
+
+function LoginComp(props) {
+    return (
+        <Container>
+            <h1>Login comp...</h1>
+        </Container>
+    );
+}
+
+export default LoginComp;
---
+++ src/Components/day_4/RegisterComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegisterComp(props) {
+    return (
+        <div>
+            <h1>Register comp...</h1>
+        </div>
+    );
+}
+
+export default RegisterComp;
